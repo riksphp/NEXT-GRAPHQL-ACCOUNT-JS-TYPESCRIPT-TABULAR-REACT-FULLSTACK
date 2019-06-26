ca4c5e3b2094e7c83141c476a66f55d7b6d11b8b
Rishi <rishi.electrical07@gmail.com>
2019-06-26 19:03:12 +0530

    Added login page

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,10 +1,69 @@
-export class LoginComponent extends Component {
+import * as React from "react";
+import { Formik } from "formik";
+import { LoginPage as TablerLoginPage } from "tabler-react";
+import { ILoginValues, ILoginError } from "../types/login.types";
+import "tabler-react/dist/Tabler.css";
+import { accountsPassword } from '../utils/account';
+import Router from 'next/router';
+
+export class LoginComponent extends React.Component {
     render() {
         return (
             <>
-                <h1 className="example">Login</h1>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: "",
+                    }}
+                    validate={(values: ILoginValues) => {
+                        // same as above, but feel free to move this into a class method now.
+                        let errors: ILoginError = {};
+                        if (!values.email) {
+                            errors.email = "Required";
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                        ) {
+                            errors.email = "Invalid email address";
+                        }
+                        return errors;
+                    }}
+                    onSubmit={async (values) => {
+                        this.setState({ error: null });
+                        try {
+                            await accountsPassword.login({
+                                password: values.password,
+                                user: {
+                                    email: values.email,
+                                },
+                            });
+                            Router.replace('/timer');
+                        } catch (err) {
+                            this.setState({ error: err.message });
+                        }
+                    }}
+                    render={({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <TablerLoginPage
+                        onSubmit={handleSubmit}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        values={values}
+                        errors={errors}
+                        touched={touched}
+                        isSubmitting={isSubmitting}
+                        />
+                    )}
+                />
             </>
         );
     }
 }
+
 export default LoginComponent;
